a660e77ec5404ba9b9c240e92d83798694e35220
Gabriel Aguiar <Aguiar.gc@hotmail.com>
2024-03-21 16:50:17 -0300

    Adicionada a funcionalidade de drag and silde à sessão related

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,9 @@
+import dragableCarousel from "./modules/dragCarousel.js"
 import navbarController from "./modules/navbar.js"
 import tabController from "./modules/tabs.js"
 
 document.addEventListener('DOMContentLoaded', () => {
     tabController()
     navbarController()
+    dragableCarousel()
 })
